a5981ec54c7b5847b5af6c4d09190a322a438963
Wayne Werner <wayne@waynewerner.com>
2014-05-15 19:34:46 -0500

    Checking for root privilages

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/sh
 
+sleep 0.1
 echo "Cool, it works!"
-
+touch cruft.txt
+sudo -n true > /dev/null 2>&1
+has_root=$?
+if [ $has_root -eq 0 ]; then 
+    echo "HEy, root!"
+else
+    echo "nope, no root"
+fi
 
 # TODO: Actually exit here
 #exit 0
