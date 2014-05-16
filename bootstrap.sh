791b9a7fa4a7246be18e890dd372d8ac1bfbd659
Wayne Werner <wayne@waynewerner.com>
2014-05-15 19:53:56 -0500

    Starting le Python script, yum!

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/sh
 
+cleanup () {
+    echo "Removing ww.files"
+    rm -rf ~/programming/ww.files
+}
+
 # Wait for wget command to finish
 sleep 0.1
 
@@ -7,21 +12,19 @@ echo "Setting up dotfiles directory"
 mkdir -p ~/programming/ww.files
 wget https://github.com/waynew/ww.files/archive/master.tar.gz
 tar -xf master.tar.gz -C ~/programming/ww.files
+rm master.tar.gz
 mv ~/programming/ww.files/ww.files-master/* ~/programming/ww.files/
 rm -rf ~/programming/ww.files/ww.files-master
 
-ls -lah ~/programming/ww.files
+echo "Checking for Python"
+python --version > /dev/null 2>&1
+has_python=$?
 
-sudo -n true > /dev/null 2>&1
-has_root=$?
-if [ $has_root -eq 0 ]; then 
-    echo "HEy, root!"
+if [ $has_python -eq 0 ]; then 
+    echo "We gots Python!"
+    python ~/programming/ww.files/setup.py
 else
-    echo "nope, no root"
+    echo "Nope - no python :( Can't automagically setup."
 fi
 
-
-# TODO: Actually exit here
-#exit 0
-echo "Removing ww.files"
-rm -rf ~/programming/ww.files
+cleanup
---
+++ setup.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+import subprocess
+
+
+#ls -lah ~/programming/ww.files
+
+#sudo -n true > /dev/null 2>&1
+#has_root=$?
+#if [ $has_root -eq 0 ]; then 
+#    echo "HEy, root!"
+#else
+#    echo "nope, no root"
+#fi
+#
+## TODO: Actually exit here
+##exit 0
+
+print("Setting up Wayne's World!")
