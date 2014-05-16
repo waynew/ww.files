aa0719d00a0f374cea8749cac49c97f743b7b435
Wayne Werner <wayne@waynewerner.com>
2014-05-15 19:45:34 -0500

    Downloading file from repo

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/sh
 
+# Wait for wget command to finish
 sleep 0.1
-echo "Cool, it works!"
-touch cruft.txt
+
+echo "Setting up dotfiles directory"
+mkdir -p ~/programming/ww.files
+wget https://github.com/waynew/ww.files/archive/master.tar.gz
+tar -xf master.tar.gz -C ~/programming/ww.files
+mv ~/programming/ww.files/ww.files-master/* ~/programming/ww.files/
+rm -rf ~/programming/ww.files/ww.files-master
+
+ls -lah ~/programming/ww.files
+
 sudo -n true > /dev/null 2>&1
 has_root=$?
 if [ $has_root -eq 0 ]; then 
@@ -11,6 +20,8 @@ else
     echo "nope, no root"
 fi
 
+
 # TODO: Actually exit here
 #exit 0
-rm cruft.txt
+echo "Removing ww.files"
+rm -rf ~/programming/ww.files
