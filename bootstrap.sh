00fd2f93ccc463dccedaceca571b38be8ee420bb
Wayne Werner <wayne@waynewerner.com>
2014-05-15 19:57:37 -0500

    better output

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/sh
 
 cleanup () {
-    echo "Removing ww.files"
+    echo "\nRemoving ww.files"
     rm -rf ~/programming/ww.files
+
+    echo "See ~/ww.files.log for more info"
 }
 
 # Wait for wget command to finish
@@ -11,20 +13,20 @@ sleep 0.1
 echo "Setting up dotfiles directory"
 mkdir -p ~/programming/ww.files
 wget https://github.com/waynew/ww.files/archive/master.tar.gz
-tar -xf master.tar.gz -C ~/programming/ww.files
+tar -xf master.tar.gz -C ~/programming/ww.files >> ~/ww.files.log 2>&1
 rm master.tar.gz
 mv ~/programming/ww.files/ww.files-master/* ~/programming/ww.files/
 rm -rf ~/programming/ww.files/ww.files-master
 
-echo "Checking for Python"
+echo "\nChecking for Python"
 python --version > /dev/null 2>&1
 has_python=$?
 
 if [ $has_python -eq 0 ]; then 
-    echo "We gots Python!"
+    echo "\nWe gots Python!"
     python ~/programming/ww.files/setup.py
 else
-    echo "Nope - no python :( Can't automagically setup."
+    echo "\nNope - no python :( Can't automagically setup."
 fi
 
 cleanup
