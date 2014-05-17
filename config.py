fbb4443c1928bb44e96bcca4b8683d76be35dec5
Wayne Werner <wayne@waynewerner.com>
2014-05-16 22:46:03 -0500

    Ugh. Functions are True, of course

--- config.py
+++ config.py
@@ -16,13 +16,14 @@ DOTFILES = ['.vimrc',
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 HASHY_PATH = os.path.join(ROOT_DIR, 'hashy.list')
 
-if os.path.isfile:
-    with open(os.path.join(ROOT_DIR, 'apikey')) as f:
+_apifile = os.path.join(ROOT_DIR, 'apikey')
+if os.path.isfile(:
+    with open(_apifile) as f:
         APIKEY = f.read().strip()
 else:
     print("Go to https://github.com/settings/applications and generate a new api key")
     apikey = input("Key: ")
-    with open(os.path.join(ROOT_DIR, 'apikey'), 'w') as f:
+    with open(_apifile, 'w') as f:
         f.write(apikey)
     APIKEY = apikey
 
