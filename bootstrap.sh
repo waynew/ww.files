e28e8c9d0d577399ebff619335bcbbc8a3abbc57
Wayne Werner <wayne@waynewerner.com>
2014-12-29 19:56:20 -0600

    It's almost downloading time!

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,21 @@
-GITHUB_USER=waynew
+#!/bin/sh
 
+GITHUB_USER=waynew
 
 command=$1
 
+function show_help(){
+    echo "Usage: "
+    echo "bootstrap.sh help         Show this message"
+    echo "bootstrap.sh              Bootstrap yer dotfiles"
+    echo "bootstrap.sh gentoken     Generate GitHub api token"
+}
+
 case $command in
     help)
-        echo "Usage: "
-        echo "bootstrap.sh help         Show this message"
-        echo "bootstrap.sh gentoken     Generate GitHub api token"
+        show_help
         ;;
+
     gentoken)
         echo "Time to generate token"
         curl https://api.github.com/authorizations \
@@ -26,12 +33,29 @@ case $command in
             echo "Manage your authorizations at https://github.com/settings/applications - this is dotfiles-cli";
         fi
         ;;
+
     upload)
         # TODO: write uploading stuff
         echo "Upload stuff"
         ;;
+
+    change-user)
+        echo $1 $2;
+        if [ "$#" -lt 2 ]; then
+            printf "Changing GITHUB_USER to: "
+            read user
+        else
+            user=$2
+        fi
+        cat $0 | sed "0,/GITHUB_USER=.*$/{s/GITHUB_USER=.*$/GITHUB_USER=$user/}" > /tmp/dotfilesbootstrap.sh
+        exec /bin/sh -c "mv /tmp/dotfilesbootstrap.sh $0 && chmod +x $0"
+        ;;
+
     *)
         # TODO: download .vimrc and other dotfiles
-        echo "It's bootstrappin' time!"
+        user=${1:-$GITHUB_USER}
+        repo=${2:-ww.files}
+        url=https://github.com/$user/$repo/archive/master.tar.gz
+        echo "Downloading dotfiles for $user from $url"
         ;;
 esac
